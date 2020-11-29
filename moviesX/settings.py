527ffefa849e35ad02e1871d482acbd4e335b088
nerdthejohn <newtonjohn043@gmail.com>
2020-11-29 23:18:20 +0100

    mapami

--- moviesX/settings.py
+++ moviesX/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y*z+_3_#i82c*5kad#4_o6pb6i*0o%e@2@oh$o7b$5+smnxd95'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
